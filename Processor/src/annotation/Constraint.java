a84dee23a5731665beedddf4dfd6365cf7066aa5
carmudgeon <diego.agudelo@payulatam.com>
2015-03-07 12:27:10 -0500

    adicion del proyecto inmobiliaria en FIDE y correccion de la anotacion Constraint

--- Processor/src/annotation/Constraint.java
+++ Processor/src/annotation/Constraint.java
@@ -3,5 +3,6 @@ package annotation;
 public @interface Constraint {
 
 	ConstraintType constraint() default ConstraintType.NULL;
+	String featureOrigin() default "";
 	String referencedFeature() default "";
 }
