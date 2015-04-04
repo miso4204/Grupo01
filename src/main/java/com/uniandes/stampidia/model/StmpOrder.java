e0301f7b349a74205de1fd4501560fca4424abf4
carmudgeon <diego.agudelo2@gmail.com>
2015-04-03 22:27:29 -0500

    se adiiciona el import faltante

--- src/main/java/com/uniandes/stampidia/model/StmpOrder.java
+++ src/main/java/com/uniandes/stampidia/model/StmpOrder.java
@@ -10,6 +10,7 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
@@ -22,6 +23,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
