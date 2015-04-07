3ea57ef0eab51b01273877320f961619592c0eb2
Tachu <tachu.salamanca@gmail.com>
2015-04-07 01:31:23 -0500

    get y post para pay desde angular completo

--- src/main/webapp/js/controllers/SelectPaymentController.js
+++ src/main/webapp/js/controllers/SelectPaymentController.js
@@ -9,9 +9,25 @@
 	    console.log($scope.paymentTypes);
 	},function(response){
 	    console.log('bad' + response);
-	})
+	});
+	$scope.submit = function(){
+		var paymentMethod;
+		if ($scope.paymenttype == 1){
+			paymentMethod = "cash"
+		} else if ($scope.paymenttype == 2){
+			paymentMethod = "pse"
+		} else if ($scope.paymenttype == 3){
+			paymentMethod = "credit"
+		}
+    	selectPaymentService.pay(paymentMethod, 6).$promise.then(function(response){
+    	    console.log(response);
+    	},function(response){
+    	    console.log('bad' + response);
+    	})
     };
-
+    }
+    
+    
     angular.module('stampidia.controllers').controller('SelectPaymentController',
 	    [ '$scope', 'selectPaymentService', 'appSettings', SelectPaymentController ]);
 }());
