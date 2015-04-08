11e91f92c4c2a24a638839c015ff6c1cf98d3939
Tachu <tachu.salamanca@gmail.com>
2015-04-07 20:28:00 -0500

    payment front

--- src/main/webapp/js/controllers/SelectPaymentController.js
+++ src/main/webapp/js/controllers/SelectPaymentController.js
@@ -19,6 +19,27 @@
             $scope.$apply();
         }
     };
+    $scope.deliveryDay = function() {
+    	var month = new Array();
+		month[0] = "January";
+		month[1] = "February";
+		month[2] = "March";
+		month[3] = "April";
+		month[4] = "May";
+		month[5] = "June";
+		month[6] = "July";
+		month[7] = "August";
+		month[8] = "September";
+		month[9] = "October";
+		month[10] = "November";
+		month[11] = "December";
+		var d = new Date();
+		var n = month[d.getMonth()];
+		var deliveryDay = d.getDate() + 5;
+		console.log(deliveryDay);
+		var fullDeliveryDate =  n + " " + deliveryDay + ", " + d.getFullYear();
+		return fullDeliveryDate;
+    };
 	$scope.submit = function(){
 		var paymentMethod;
 		if ($scope.paymenttype == 1){
@@ -28,7 +49,7 @@
 		} else if ($scope.paymenttype == 3){
 			paymentMethod = "credit"
 		}
-    	selectPaymentService.pay(paymentMethod, 6).$promise.then(function(response){
+    	selectPaymentService.pay(paymentMethod, 1).$promise.then(function(response){
     	    console.log(response);
     	    $scope.changeRoute('#/' + response.mensajeAccion);
     	    console.log($location);
@@ -37,8 +58,6 @@
     	})
     };
     }
-    
-    
     angular.module('stampidia.controllers').controller('SelectPaymentController',
 	    [ '$scope', '$location', 'selectPaymentService', 'appSettings', SelectPaymentController ]);
 }());
