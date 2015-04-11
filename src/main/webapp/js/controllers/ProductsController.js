2cd0489a1adcaa4ff4f3d793419c64a69ee219b9
carmudgeon <diego.agudelo2@gmail.com>
2015-04-10 23:34:23 -0500

    Ayuda a mauricio

--- src/main/webapp/js/controllers/ProductsController.js
+++ src/main/webapp/js/controllers/ProductsController.js
@@ -2,9 +2,35 @@
     'use strict';
 
     var ProductsController = function($scope, productsService, categoriesService, appSettings) {
-	$scope.products = productsService.listProducts();
-	$scope.categories = categoriesService.listCategories();
-	console.log($scope.products);
+	$scope.listProducts = function(id){	    
+	    productsService.listProducts(id).$promise.then(
+		    function(response){
+			$scope.products = response.resultado;		    
+		    }, function(response){
+			console.log(response);
+		    }    
+	    );
+	}
+	$scope.listCategories = function(){
+	    categoriesService.listCategories().$promise.then(		
+		    function(response){
+			$scope.categories= response.resultado;
+		    },
+		    function(response){
+			console.log(response)
+		    })
+	}
+	
+	$scope.selectCategory = function(productId){
+	    $scope.listProducts(productId);
+	}
+	
+	var init = function(){
+	    $scope.listCategories();
+	    $scope.listProducts();
+	}
+	init();
+	
     };
     angular.module('stampidia.controllers').controller('ProductsController', [ '$scope', 'productsService', 'categoriesService', 'appSettings', ProductsController ]);
 
