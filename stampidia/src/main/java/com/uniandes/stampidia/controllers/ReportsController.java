edda79d1167563935f66d09ecfcad6736e85dd37
carmudgeon <diego.agudelo@payulatam.com>
2015-05-09 17:17:32 -0500

    STMP-30 Se corrigen los servicios

--- stampidia/src/main/java/com/uniandes/stampidia/controllers/ReportsController.java
+++ stampidia/src/main/java/com/uniandes/stampidia/controllers/ReportsController.java
@@ -3,6 +3,7 @@ package com.uniandes.stampidia.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.uniandes.stampidia.services.ReportService;
@@ -17,16 +18,16 @@ public class ReportsController {
 
 	
 	@RequestMapping(value="/report_by_period",method=RequestMethod.GET)
-	public Resultado getReportByPeriod(){
+	public Resultado getReportByPeriod(@RequestParam(value="username", required = true) String username){
 		Resultado resultado = new Resultado();
-		resultado.setResultado(reportsService.reportByPeriod());
+		resultado.setResultado(reportsService.reportByPeriod(username));
 		return resultado;
 	}
 	
 	@RequestMapping(value="/report_by_sales",method=RequestMethod.GET)
-	public Resultado getReportBySales(){
+	public Resultado getReportBySales(@RequestParam(value="username", required = true) String username){
 		Resultado resultado = new Resultado();
-		resultado.setResultado(reportsService.reportBySales());
+		resultado.setResultado(reportsService.reportBySales(username));
 		return resultado;
 	}
 }
